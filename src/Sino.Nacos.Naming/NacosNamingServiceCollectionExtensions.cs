af7739338b687a26bf597ad14c6ae1206e56e130
姚正发 <yaozhengfa@vip56.cn>
2019-09-20 13:39:07 +0800

    增加asp.net core注入支持

---
+++ src/Sino.Nacos.Naming/NacosNamingServiceCollectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Sino.Nacos.Naming;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class NacosNamingServiceCollectionExtensions
+    {
+        /// <summary>
+        /// 启动服务注册与发现
+        /// </summary>
+        public static IServiceCollection AddNacosNaming(this IServiceCollection services, IConfigurationSection section)
+        {
+            var mainCfg = new NamingConfig();
+            section.Bind(mainCfg);
+            return AddNacosNaming(services, mainCfg);
+        }
+
+        /// <summary>
+        /// 启动服务注册与发现
+        /// </summary>
+        public static IServiceCollection AddNacosNaming(this IServiceCollection services, NamingConfig config)
+        {
+            services.AddSingleton(config);
+            services.AddSingleton<INamingService, NacosNamingService>();
+
+            return services;
+        }
+    }
+}
