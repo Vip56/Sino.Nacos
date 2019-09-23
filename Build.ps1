e27d29ce4499875ede90b240ead853541607fbd7
姚正发 <yaozhengfa@vip56.cn>
2019-09-23 09:16:56 +0800

    修改发包方式

--- Build.ps1
+++ Build.ps1
@@ -18,6 +18,6 @@ exec { & dotnet restore }
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = [convert]::ToInt32($revision, 10)
 
-exec { & dotnet pack .\src\Sino.Nacos.Naming -c Release -o .\artifacts --version-suffix=$revision }
+exec { & dotnet pack .\src\Sino.Nacos.Naming -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg -c Release -o .\artifacts --version-suffix=$revision }
 
-exec { & dotnet nuget push .\src\Sino.Nacos.Naming\artifacts\*.nupkg -k oy2bsfqlhnjxps44xdlqc3czk7z5seye3jukjxiorixdui -s https://api.nuget.org/v3/index.json }
+exec { & dotnet nuget push .\src\Sino.Nacos.Naming\artifacts\*.snupkg -k oy2bsfqlhnjxps44xdlqc3czk7z5seye3jukjxiorixdui -s https://api.nuget.org/v3/index.json }
