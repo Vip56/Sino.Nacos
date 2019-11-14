b7759e6d680699eee68f79c3fb01252f96d9ff55
姚正发 <yaozhengfa@vip56.cn>
2019-11-14 10:07:36 +0800

    修改构建脚本

--- Build.ps1
+++ Build.ps1
@@ -20,4 +20,8 @@ $revision = [convert]::ToInt32($revision, 10)
 
 exec { & dotnet pack .\src\Sino.Nacos.Naming -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg -c Release -o .\artifacts --version-suffix=$revision }
 
+exec { & dotnet pack .\src\Sino.Nacos.Config -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg -c Release -o .\artifacts --version-suffix=$revision }
+
 exec { & dotnet nuget push .\src\Sino.Nacos.Naming\artifacts\*.snupkg -k oy2bsfqlhnjxps44xdlqc3czk7z5seye3jukjxiorixdui -s https://api.nuget.org/v3/index.json }
+
+exec { & dotnet nuget push .\src\Sino.Nacos.Config\artifacts\*.snupkg -k oy2bsfqlhnjxps44xdlqc3czk7z5seye3jukjxiorixdui -s https://api.nuget.org/v3/index.json }
